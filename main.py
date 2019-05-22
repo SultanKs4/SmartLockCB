241c60924e2d25de3d658e770c9c18961f4134e3
Ardan Anjung Kusuma <ardananjungkusuma@gmail.com>
2019-05-22 13:15:05 +0700

    uploading main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as IO
+import time
+from firebase import firebase
+from gpiozero import Servo
+from time import sleep
+
+fire = firebase.FirebaseApplication('https://smartlock-01.firebaseio.com/',None)
+
+PIN_IR = 25
+PIN_SERVO = Servo(14)
+
+IO.setmode(IO.BCM)
+IO.setup(PIN_IR, IO.IN)
+
+
+while True:
+        infrared=fire.get('/infra',None)
+        servo2=fire.get('/servo2',None)
+        if infrared == 1:
+                print("Infrared Activated")
+                if (IO.input(PIN_IR) == True) :
+                        sleep(3)
+                        print("Object Detected")
+                        if servo2 == 0 :
+                                print("Locker is Locked")
+                                PIN_SERVO.min()
+                        elif servo2 == 1:
+                                print("Locker is Open")
+                                PIN_SERVO.max()
+                        else :
+                                print("Enter a Valid Command")
+                else:
+                        print("No Object Detected")
+                        print("Waiting 3 Second to Detect the Setting")
+                        sleep(3)
+        else:
+                print("Infra Wasnt Activated")
+                print("Waiting 5 Second to Reload the Setting")
+                sleep(5)
