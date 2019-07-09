e77579c7651d96fe878b74d9015cd00162ab283d
Sultan Achmad Qum Masykuro NS <44137684+SultanKs4@users.noreply.github.com>
2019-07-09 09:14:07 +0700

    Delete firebase_cred.py

--- firebase_cred.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-cred = credentials.Certificate('cred/serviceAccountKey.json')
-firebase_admin.initialize_app(cred)
-firestore_db = firestore.client()
-smart_lock_col = firestore_db.collection(u'smartlock')
